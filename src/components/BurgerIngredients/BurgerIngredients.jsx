03f3ffe19111991e694ab5cff6a254c744788362
Mikhail Zakharov <erreal@yandex.ru>
2022-11-27 12:49:36 +0300

    Update BurgerIngredients.jsx

--- src/components/BurgerIngredients/BurgerIngredients.jsx
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -1,25 +1,48 @@
 import React from 'react';
-import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Tab, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import ingredientsStyles from './BurgerIngredients.module.css';
+import data from '../../utils/data';
 
 const BurgerIngredients = (props) => {
     const [current, setCurrent] = React.useState('one');
+    const ingredientType = [
+        {
+            name: 'Булки',
+            type: 'bun'
+        },
+        {
+            name: 'Начинки',
+            type: 'main'
+        },
+        {
+            name: 'Соусы',
+            type: 'sauce'
+        }
+    ];
+    const dataSorted = props.data.sort((first, second) => first['type'] > second['type'] ? 1 : -1);
+    console.log(dataSorted);
     return (
         <section className={ingredientsStyles.mainsection}>
             <h2 className="text text_type_main-large mt-10 mb-5">Соберите бургер</h2>
             <div style={{ display: 'flex' }}>
                 <Tab value="one" active={current === 'one'} onClick={setCurrent}>
-                    Булки
+                    {ingredientType[0].name}
                 </Tab>
                 <Tab value="two" active={current === 'two'} onClick={setCurrent}>
-                    Соусы
+                    {ingredientType[1].name}
                 </Tab>
                 <Tab value="three" active={current === 'three'} onClick={setCurrent}>
-                    Начинки
+                    {ingredientType[2].name}
                 </Tab>
             </div>
             <div className={ingredientsStyles.ingredientsection}>
-                <IngredientsGroup />
+            {ingredientType.map((item, index) =>(
+                <IngredientsGroup key={index} type={item.type} name={item.name} data={
+                    dataSorted.filter(obj => {
+                        return obj.type === item.type
+                      })
+                }/>
+            ))}
             </div>
         </section>
     );
@@ -29,8 +52,16 @@ const IngredientsGroup = (props) => {
     return (
         <section className='mt-10'>
             <h3 className="text text_type_main-medium">
-                {props.groupTitle}
+                {props.name}
             </h3>
+            {data.map(item => (
+                <IngredientCard 
+                    key = {item.id}
+                    imageSrc = {item.image}
+                    name = {item.name}
+                    price = {item.price}
+                />
+            ))}
 
         </section>
     );
@@ -39,7 +70,11 @@ const IngredientsGroup = (props) => {
 
 const IngredientCard = (props) => {
     <div>
-        <img src={props.imageSrc} />
+        <img src={props.imageSrc} alt=""/>
+        <div className={ingredientsStyles.priceblock}>
+            <span>{props.price}</span>
+            <CurrencyIcon type="primary" />
+        </div>
     </div>
 }
 
