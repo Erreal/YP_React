436222258e7eb8a1b32293b445ba592c7eaa4012
Mikhail Zakharov <erreal@yandex.ru>
2022-12-03 22:15:23 +0300

    finished modals

--- src/components/BurgerIngredients/BurgerIngredients.jsx
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -1,18 +1,17 @@
 import React, {useRef, useEffect} from 'react';
-import { Tab, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import ingredientsStyles from './BurgerIngredients.module.css';
 import PropTypes from "prop-types";
 import ingredientType from '../../utils/types';
-
-
+import IngredientCard from '../IngredientCard/IngredientCard'
 
 const BurgerIngredients = (props) => {
-    const [current, setState] = React.useState('one');
-    const scrollRefs = useRef(current);
+    const [currentTab, setCurrentTab] = React.useState('one');
+    const scrollRefs = useRef(currentTab);
     
     useEffect(() => {
         scrollRefs.current.scrollIntoView({ behavior: "smooth" });
-    });
+    }, [currentTab]);
     const ingredientType = [
         {
             name: 'Булки',
@@ -35,26 +34,28 @@ const BurgerIngredients = (props) => {
         <section className={ingredientsStyles.mainsection}>
             <h2 className="text text_type_main-large mt-10 mb-5">Соберите бургер</h2>
             <div className={ingredientsStyles.tabs}>
-                <Tab value="one" active={current === 'one'} onClick={setState}>
+                <Tab value="one" active={currentTab === 'one'} onClick={setCurrentTab}>
                     {ingredientType[0].name}
                 </Tab>
-                <Tab value="two" active={current === 'two'} onClick={setState}>
+                <Tab value="two" active={currentTab === 'two'} onClick={setCurrentTab}>
                     {ingredientType[1].name}
                 </Tab>
-                <Tab value="three" active={current === 'three'} onClick={setState}>
+                <Tab value="three" active={currentTab === 'three'} onClick={setCurrentTab}>
                     {ingredientType[2].name}
                 </Tab>
             </div>
             <div className={ingredientsStyles.scrollsection}>
                 {ingredientType.map(item => 
-                    (<section className='mt-10' ref={current === item.place ? scrollRefs : null} key={item.type}>
+                    (<section className='mt-10' ref={currentTab === item.place ? scrollRefs : null} key={item.type}>
                         <h3 className="text text_type_main-medium">
                             {item.name}
                         </h3>
                         <div className={ingredientsStyles.groupinner}>
                             {props.data.filter(obj => {
                                 return obj.type === item.type}).map(item => (
-                                <IngredientCard key={item._id} imageSrc={item.image} name={item.name} price={item.price} />
+                                <IngredientCard 
+                                    key={item._id} 
+                                    item={item} />
                             ))}
                         </div>
                     </section>
@@ -68,22 +69,5 @@ BurgerIngredients.propTypes = {
     data: PropTypes.arrayOf(ingredientType).isRequired
 }
 
-const IngredientCard = (props) => {
-    return (
-        <div className={`${ingredientsStyles.card} mt-6 mb-8`}>
-            <img className={ingredientsStyles.cardimage} src={props.imageSrc} alt={props.name}/>
-            <div className={ingredientsStyles.cardprice}>
-                <span className="text text_type_digits-default">{props.price}</span>
-                <CurrencyIcon type="primary" />
-            </div>
-            <p className={ingredientsStyles.cardname}>{props.name}</p>
-        </div>
-    );
-}
 
-IngredientCard.propTypes = {
-    imageSrc: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-}
 export default BurgerIngredients;
---
+++ src/components/IngredientCard/IngredientCard.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import ingredientCardStyles from './IngredientCard.module.css';
+import {  CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import IngredientDetails from '../IngredientDetails/IngredientDetails';
+import Modal from '../Modal/Modal';
+import ingredientType from '../../utils/types';
+
+const IngredientCard = (props) => {
+    const [modal, setModal] = React.useState({visible:false});    
+    const handleOpenModal = () => {
+        setModal({ visible: true });
+    }
+    
+    const handleCloseModal = () => {
+        setModal({ visible: false });
+    }
+    return (
+        <>
+            <div className={`${ingredientCardStyles.card} mt-6 mb-8`} onClick={handleOpenModal}>
+                <img className={ingredientCardStyles.cardimage} src={props.item.image} alt={props.item.name}/>
+                <div className={ingredientCardStyles.cardprice}>
+                    <span className="text text_type_digits-default">{props.item.price}</span>
+                    <CurrencyIcon type="primary" />
+                </div>
+                <p className={ingredientCardStyles.cardname}>{props.item.name}</p>
+            </div>
+            {modal.visible && 
+                <Modal onClose={handleCloseModal} title='Детали ингредиента'>
+                    <IngredientDetails 
+                        image = {props.item.image_large}
+                        name = {props.item.name}
+                        calories = {props.item.calories}
+                        proteins = {props.item.proteins}
+                        fat = {props.item.fat}
+                        carbohydrates = {props.item.carbohydrates}
+                    />
+                </Modal>
+            }
+        </>
+    );
+}
+
+IngredientCard.propTypes = {
+    item: ingredientType.isRequired,
+}
+
+export default IngredientCard;
